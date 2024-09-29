75c184a989b95789d2f7767b2f1a3f156c3b0848
Fausto De Leon <fdeleon@colgate.edu>
2024-09-28 21:12:25 -0400

    FlashCard generation done

---
+++ FlashCard.cs
@@ -0,0 +1,10 @@
+﻿namespace Memoreyes {
+    public class FlashCard {
+        public string Title { get; set;}
+        public string Description { get; set;}
+        public FlashCard(string Title, string Description) { 
+            this.Title = Title;
+            this.Description = Description;
+        }
+    }
+}
